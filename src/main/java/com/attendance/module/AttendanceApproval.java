0201f97aa0215e93994976c24ba422f0f4202ce3
vikeyqyq <vikeyqyq@163.com>
2019-05-17 18:31:06 +0800

    考勤模块实体

---
+++ src/main/java/com/attendance/module/AttendanceApproval.java
@@ -0,0 +1,19 @@
+package com.attendance.module;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.common.base.module.BaseModule;
+
+/**
+ * 
+ * @Description 考勤审批表
+ * @author qinyongqian
+ * @date 2019年5月17日
+ *
+ */
+@Entity
+@Table(name = "KQ_ATTENDANCE_APPROVAL")
+public class AttendanceApproval extends BaseModule{
+
+}
---
+++ src/main/java/com/attendance/module/ChangeShifts.java
@@ -0,0 +1,19 @@
+package com.attendance.module;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.common.base.module.BaseModule;
+
+/**
+ * 
+ * @Description 换班表
+ * @author qinyongqian
+ * @date 2019年5月17日
+ *
+ */
+@Entity
+@Table(name = "KQ_CHANGE_SHIFTS")
+public class ChangeShifts extends BaseModule{
+
+}
